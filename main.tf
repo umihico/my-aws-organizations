12d76ea9ede44d80a4e1a4d7c10a956fc9837d90
umihico <umihico@users.noreply.github.com>
2022-01-25 07:14:08 +0900

    feat: enable cloudtrail.amazonaws.com & config.amazonaws.com

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ locals {
 
 resource "aws_organizations_organization" "org" {
   aws_service_access_principals = [
-    # "cloudtrail.amazonaws.com",
-    # "config.amazonaws.com"
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com"
   ]
   enabled_policy_types = [
     "SERVICE_CONTROL_POLICY"
